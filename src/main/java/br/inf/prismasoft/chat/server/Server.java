8d8fe08522064eec548258d0c4215515188291ec
Ricardo M. Oliveira <ricardo.martinelli.oliveira@gmail.com>
2016-05-20 12:01:43 -0300

    Commit Inicial

---
+++ src/main/java/br/inf/prismasoft/chat/server/Server.java
@@ -0,0 +1,50 @@
+package br.inf.prismasoft.chat.server;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Server extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Server frame = new Server();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Server() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+		int largura = (int) (tela.getWidth() - 640) / 2;
+		int altura = (int) (tela.getHeight() - 535) / 2;
+		setSize(640, 505);
+		
+		setLocation(largura, altura);
+		setContentPane(contentPane);
+	}
+
+}
